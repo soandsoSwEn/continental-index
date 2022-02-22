e4963da8ee4619c9dc53b0699982044e77ef54b9
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2022-02-22 23:10:17 +0200

    added HromovIndex class

---
+++ src/HromovIndex.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Soandso\ContinentalIndex;
+
+/**
+ * HromovIndex class contains a method for calculating the continentality index using the Khromov method
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class HromovIndex implements ContinentalIndexInterface
+{
+    /**
+     * Returns the continentality index calculated using the Khromov method
+     *
+     * @param float $tempAmplitude Temperature amplitude value
+     * @param float $longitude The value of geographic latitude
+     * @return float Continentality index
+     */
+    public function calcIndex(float $tempAmplitude, float $longitude) : float
+    {
+        return round((($tempAmplitude - 5.4 * sin(deg2rad($longitude))) / $tempAmplitude), 2);
+    }
+}
